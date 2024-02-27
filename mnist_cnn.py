38d0840fbb48e406d6a28959359856989454dcf8
Isaac Berlin <isaacmorrisberlin@gmail.com>
2024-02-27 13:16:00 -0600

    Refactor CNN train and test code

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -28,9 +28,6 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=64, s
 
 SIZE_IMG = len(train_dataset[0][0])
 NUM_OUTCOMES = 10 # 10 digits as possible outcomes (0,1,2,3,4,5,6,7,8,9)
-
-print(train_dataset[0][0].shape)
-print('Size of the image: {}'.format(SIZE_IMG))
       
 model = nn.Sequential(
           nn.Conv2d(1,20,5),
@@ -70,4 +67,49 @@ class CNN(torch.nn.Module):
     
 # Create the model
 model = CNN(28*28, NUM_OUTCOMES).to(device)
+
+# Loss and optimizer
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# Train the model
+n_total_steps = len(train_loader)
+num_epochs = 2
+
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # origin shape: [64, 1, 28, 28]
+        # resized: [64, 784]
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        
+        # Backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if (i+1) % 100 == 0:
+            print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item()}')
+            
+
+# Test the model
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        
+        # max returns (value, index)
+        _, predicted = torch.max(outputs, 1)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+        
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network on the 10000 test images: {acc} %')
 
